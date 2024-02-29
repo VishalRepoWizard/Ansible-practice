7858688a0758ba4c1663cfb7df2137c4876f20b4
vishal <rajput.vishal.rajes@gmail.com>
2024-02-29 10:49:48 +0530

    scriptmodule

--- practice/module/script/testscript.sh
+++ practice/module/script/testscript.sh
@@ -1,5 +1,2 @@
 #!/bin/bash
-pwd >> /tmp/filecreatedbyscript.txt
-echo "first arg: $1" >> /tmp/filecreatedbyscript.txt
 echo "===========================" >> /tmp/filecreatedbyscript.txt
-ifconfig >> /tmp/filecreatedbyscript.txt
